84cf14272d8bf03c7d3aee362ca5408ae43d4bbe
stavros <stav@stav.cc>
2022-09-19 13:05:45 -0700

    .

--- src/evloop.rs
+++ src/evloop.rs
@@ -34,48 +34,76 @@ fn epoll_create() -> io::Result<EpollFd> {
     Ok(EpollFd{fd})
 }
 
-fn epoll_ctl(epoll_fd: EpollFd, op: i32, fd: RawFd, mut event: Event) -> io::Result<()> {
+fn epoll_ctl(epoll_fd: &EpollFd, op: i32, fd: RawFd, mut event: Event) -> io::Result<()> {
     libccall!(epoll_ctl(epoll_fd.fd, op, fd, &mut event as *mut _ as *mut libc::epoll_event))?;
     Ok(())
 }
 
-fn epoll_wait(epoll_fd: EpollFd,  events: &mut Vec<Event>, timeout: i64) -> io::Result<()> {
+fn epoll_wait(epoll_fd: &EpollFd,  events: &mut Vec<Event>, timeout: i64) -> io::Result<()> {
     let max_events = events.capacity();
     libccall!(epoll_wait(epoll_fd.fd, events.as_mut_ptr() as *mut libc::epoll_event,
 			max_events as libc::c_int, timeout as libc::c_int))?;
     Ok(())
 }
 
-struct Watcher<T> {
+struct FDWatcher<F, T> where F: AsRawFd {
+    fd: F,
     object: T,
-    callback: fn(&mut T, &mut EvLoop),
+    callback: fn(&mut T, &mut EvLoop, &mut F),
+    callback_thunk: fn(&mut FDWatcher<F, T>, &mut EvLoop),
 }
 
-trait Dispatchable {
-    fn dispatch(&mut self, el: &mut EvLoop);
-}
-
-impl<T> Dispatchable for Watcher<T> {
-    fn dispatch(&mut self, el: &mut EvLoop) {
-	(self.callback)(&mut self.object, el);
+impl<F, T> FDWatcher<F, T> where F: AsRawFd {
+    fn cb_thunk(&mut self, ev: &mut EvLoop) {
+	(self.callback)(&mut self.object, ev, &mut self.fd);
+	
+    }
+    fn new(fd: F, object: T, callback: fn(&mut T, &mut EvLoop, &mut F)) -> FDWatcher<F, T> {
+	FDWatcher{
+	    fd,
+	    object,
+	    callback,
+	    callback_thunk: Self::cb_thunk,
+	}
     }
 }
 
-trait DispatchableFd: Dispatchable + AsRawFd {} 
-type Ev = dyn DispatchableFd ;
+
+trait FDDispatchable {
+    fn fd(&mut self) -> &mut dyn AsRawFd;
+    fn dispatch(&mut self, el: &mut EvLoop);
+}
+
+impl<F, T> FDDispatchable for FDWatcher<F, T> where F: AsRawFd {
+    fn fd(&mut self) -> &mut dyn AsRawFd {
+	&mut self.fd
+    }
+    fn dispatch(&mut self, el: &mut EvLoop) {
+	(self.callback_thunk)(self, el);
+    }
+}
 
 struct EvLoop {
     fd: EpollFd,
     token_gen: TokenGen,
-    watchers: Vec<Rc<RefCell<dyn Dispatchable>>>,
+    watchers: Vec<Rc<RefCell<dyn FDDispatchable>>>,
 }
 
 impl EvLoop {
-    fn new() -> io::Result<EvLoop> {
+    pub fn new() -> io::Result<EvLoop> {
 	Ok(EvLoop{
 	    fd: epoll_create()?,
 	    token_gen: TokenGen::new(),
 	    watchers: Vec::new(),
 	})
+    }
+
+    pub fn watch<F: 'static, T: 'static>(&mut self, fd: F, object: T, callback: fn(&mut T, &mut EvLoop, &mut F))
+    where F: AsRawFd {
+	epoll_ctl(&self.fd, 0, fd.as_raw_fd(), Event{events: libc::EPOLLIN as u32, data: 0});
+	self.watchers.push(Rc::new(RefCell::new(FDWatcher::new(fd, object, callback))));
+    }
+
+    pub fn run(&mut self) {
     }
 }
